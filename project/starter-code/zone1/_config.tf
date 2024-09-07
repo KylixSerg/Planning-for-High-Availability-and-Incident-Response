260accc0881b16aa9294e1f11533c73a27fceaaf
Abdallah Serghine <abdallah.serghine@olx.pl>
2024-09-07 23:57:10 +0200

    Add bucket

--- project/starter-code/zone1/_config.tf
+++ project/starter-code/zone1/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-<user_name>"
+     bucket = "udacity-tf-sabdallah"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
